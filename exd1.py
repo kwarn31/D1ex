7001c565d2c8c4bb6b6dc54eff19c1a08fb98211
kwarn31 <kristin31w@gmail.com>
2025-03-08 14:36:12 -0500

    Update exd1.py

--- exd1.py
+++ exd1.py
@@ -24,9 +24,12 @@ if category_filter != 'All':
     df = df[df['Category'] == category_filter]
                                      
 # KPIs 
-st.title("E-commerce Sales Dashboard") 
-st.metric("Total Revenue", f"${df['Total'].sum():,.2f}") 
-st.metric("Total Orders", len(df))       
+st.title("E-commerce Sales Dashboard")
+col1, col2 =st.columns(2)
+with col1: 
+    st.metric("Total Revenue", f"${df['Total'].sum():,.2f}") 
+with col2: 
+    st.metric("Total Orders", len(df))       
 
 # Data Table 
 st.subheader("Sales Data")
